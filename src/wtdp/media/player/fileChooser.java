6cf2706948fcea720296b5eb3dcab6493e562dd6
Developer-Pratik-Baisware <pybaisware1947@gmail.com>
2021-07-04 11:01:45 +0530

    Initial Commit

---
+++ src/wtdp/media/player/fileChooser.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package wtdp.media.player;
+
+import java.io.File;
+
+/**
+ *
+ * @author Pratik Baisware pb7
+ */
+class fileChooser {
+
+    static Object getExtensionFilters() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    static File showOpenDialog(Object object) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
